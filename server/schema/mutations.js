5b0670c5f7054d97da84a8a4606abe71c83ddf2e
Mauricio Mercado <mau@maumercado.com>
2017-12-09 15:35:12 -0800

    Add user mutations to signup login and logout

---
+++ server/schema/mutations.js
@@ -0,0 +1,50 @@
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString } = graphql;
+const UserType = require("./types/user_type");
+const AuthService = require("../services/auth");
+
+const mutation = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        signup: {
+            type: UserType,
+            args: {
+                email: { type: GraphQLString },
+                password: { type: GraphQLString }
+            },
+            // req|ctx is the request coming
+            // from express
+            async resolve(parentValue, { email, password }, req) {
+                try {
+                    return await AuthService.signup({ email, password, req });
+                } catch (e) {
+                    throw new Error(e);
+                }
+            }
+        },
+        login: {
+            type: UserType,
+            args: {
+                email: { type: GraphQLString },
+                password: { type: GraphQLString }
+            },
+            async resolve(parentValue, { email, password }, req) {
+                try {
+                    return await AuthService.login({ email, password, req });
+                } catch (err) {
+                    throw new Error(err);
+                }
+            }
+        },
+        logout: {
+            type: UserType,
+            resolve(parentValue, args, req) {
+                const { user } = req;
+                req.logout();
+                return user;
+            }
+        }
+    }
+});
+
+module.exports = mutation;
