1c6e871624e8d68e098695919c0ad29878510833
Mauricio Mercado <mau@maumercado.com>
2017-12-09 15:32:40 -0800

    Added db to config

--- config/index.js
+++ config/index.js
@@ -3,6 +3,7 @@ module.exports = {
         port: process.env.PORT || 4000,
         log: {
             level: process.env.NODE_LEVEL
-        }
+        },
+        db: process.env.MONGO_URI
     }
 };
