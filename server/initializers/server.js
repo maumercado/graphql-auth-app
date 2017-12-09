ef2eb9134c776a15fbad3c7b2ea221bd078e2758
Mauricio Mercado <mau@maumercado.com>
2017-12-09 15:31:54 -0800

    Changed server to express to make things easier to set up with passport
    Add Graphql libs
    Add session handler libs for express
    Add bunyan for express

--- server/initializers/server.js
+++ server/initializers/server.js
@@ -1,68 +1,67 @@
-const Hapi = require("hapi");
+const express = require("express");
+const aaWrapper = require("express-async-await");
+const path = require("path");
+const bunyanMiddleware = require("bunyan-middleware");
+const session = require("express-session");
+const passport = require("passport");
+const MongoStore = require("connect-mongo")(session);
 const config = require("../../config");
 const log = require("./log");
-
-const server = new Hapi.Server({
-    port: config.server.port,
-    state: {
-        strictHeader: false
-    },
-    routes: { cors: { origin: ["*"] } }
-});
+const routerV1 = require("../v1/routes");
 
 const initServer = async () => {
     try {
-        await server.register([
-            { plugin: require("inert") },
-            { plugin: require("vision") },
-            { plugin: require("../plugins/bunyan_logging"), options: { log } }
-        ]);
+        const server = aaWrapper(express());
 
-        server.views({
-            engines: {
-                html: require("handlebars")
-            },
-            relativeTo: __dirname,
-            path: "../client/build/"
+        server.use(
+            bunyanMiddleware({
+                headerName: "X-Request-Id",
+                propertyName: "reqId",
+                logName: "req_id",
+                obscureHeaders: ["Authorization"],
+                logger: log
+            })
+        );
+
+        // Configures express to use sessions.  This places an encrypted identifier
+        // on the users cookie.  When a user makes a request, this middleware examines
+        // the cookie and modifies the request object to indicate which user made the request
+        // The cookie itself only contains the id of a session; more data about the session
+        // is stored inside of MongoDB.
+        server.use(
+            session({
+                resave: true,
+                saveUninitialized: true,
+                secret: "aaabbbccc",
+                store: new MongoStore({
+                    url: config.server.db,
+                    autoReconnect: true
+                })
+            })
+        );
+
+        // Passport is wired into express as a middleware. When a request comes in,
+        // Passport will examine the request's session (as set by the above config) and
+        // assign the current user to the 'req.user' object.  See also servces/auth.js
+        server.use(passport.initialize());
+        server.use(passport.session());
+
+        // Instruct Express to pass on any request made to the '/graphql' route
+        // to the GraphQL instance.
+        server.use(routerV1.graphql);
+        server.use(express.static(path.resolve(__dirname, "../../client", "build")));
+        server.use(routerV1.react);
+
+        server.use((err, req, res, next) => {
+            log.error({ err }, "Server Error");
+            res.status(500).send("Ouch!");
         });
 
-        server.ext("onPostHandler", (request, h) => {
-            if (request.response.isBoom && request.response.output.statusCode === 404) {
-                return h.view("404.html", {
-                    config: JSON.stringify(config.client)
-                });
-            }
-
-            return h.continue;
+        server.listen(config.server.port, () => {
+            log.info({ ...server.info }, `Server Started on port ${config.server.port}`);
         });
-
-        const ReactRoutes = [
-            {
-                method: "GET",
-                path: "/{param*}",
-                handler: {
-                    directory: {
-                        path: "client/build"
-                    }
-                }
-            },
-            {
-                method: "GET",
-                path: "/",
-                handler(request, h) {
-                    return h.view("index.html", {
-                        config: JSON.stringify(config.client)
-                    });
-                }
-            }
-        ];
-
-        server.route(ReactRoutes);
-
-        await server.start();
-        log.info({ info: server.info }, "Server started");
     } catch (e) {
-        log.error({ err: e }`Error ${e.message}`);
+        log.error({ err: e }`Error ${e}`);
         throw new Error(e);
     }
 };
