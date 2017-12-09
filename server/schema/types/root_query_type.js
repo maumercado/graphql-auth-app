1b90e808d56c6673eaca425f7ca14b560d985709
Mauricio Mercado <mau@maumercado.com>
2017-12-09 15:35:34 -0800

    Add mutations support and dummy fields to root query

---
+++ server/schema/types/root_query_type.js
@@ -0,0 +1,11 @@
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLID } = graphql;
+
+const RootQueryType = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        dummyField: { type: GraphQLID }
+    }
+});
+
+module.exports = RootQueryType;
