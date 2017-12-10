d6384a4d8e9dacd21e01ad1d46867aa549bfd6f2
Mauricio Mercado <mau@maumercado.com>
2017-12-09 22:01:18 -0800

    Add signup route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { ApolloProvider } from "react-apollo";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
+
 import apolloClient from "../apolloClient";
+
 import Header from "./layout/Header";
 import Main from "./Main";
 import LoginForm from "./LoginForm";
+import SignupForm from "./SignupForm";
 
 const App = () => {
     return (
@@ -15,6 +18,7 @@ const App = () => {
                     <Switch>
                         <Route exact path="/" component={Main} />
                         <Route path="/login" component={LoginForm} />
+                        <Route path="/signup" component={SignupForm} />
                     </Switch>
                 </div>
             </BrowserRouter>
