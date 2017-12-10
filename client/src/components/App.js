726afdf1d5a1c889ef3389d31984f0b7abea3185
Mauricio Mercado <mau@maumercado.com>
2017-12-09 21:23:56 -0800

    Add container class to app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,15 +4,17 @@ import { Route, Switch, BrowserRouter } from "react-router-dom";
 import apolloClient from "../apolloClient";
 import Header from "./layout/Header";
 import Main from "./Main";
+import LoginForm from "./LoginForm";
 
 const App = () => {
     return (
         <ApolloProvider client={apolloClient}>
             <BrowserRouter>
-                <div>
+                <div className="container">
                     <Header />
                     <Switch>
                         <Route exact path="/" component={Main} />
+                        <Route path="/login" component={LoginForm} />
                     </Switch>
                 </div>
             </BrowserRouter>
