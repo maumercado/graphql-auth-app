6118aba46d4f9efee51d17f076ac88c0e2b48748
Mauricio Mercado <mau@maumercado.com>
2017-12-09 19:39:26 -0800

    Initial Authentication functionality in header

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -3,8 +3,24 @@ import { graphql } from "react-apollo";
 import currentUser from "../../queries/currentUser";
 
 class Header extends Component {
+    renderButtons() {
+        const { user, loading } = this.props.data;
+        if (loading) {
+            return;
+        }
+
+        if (user) {
+            return <div>Logout</div>;
+        } else {
+            return <div>Signup/Login</div>;
+        }
+    }
     render() {
-        return <div>Header</div>;
+        return (
+            <nav>
+                <div className="nav-wrapper">{this.renderButtons()}</div>
+            </nav>
+        );
     }
 }
 
